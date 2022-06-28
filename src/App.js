b8ba867ce5070a5824b8903d6dbc9c83c99166c0
hung_nguyendang <hung.nguyendang@messagemedia.com>
2022-06-28 15:09:16 +0700

    convert to iframe test app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [src, setSrc] = useState('https://iframe-demo-server.herokuapp.com')
+    const [url, setUrl] = useState('https://iframe-demo-server.herokuapp.com')
+    const handleOnChange = (e) => setUrl(e.target.value)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          iFrame app with Content-Security-Policy: frame-ancestors
+          Enter URL
         </p>
-        <iframe src="https://iframe-demo-server.herokuapp.com" />
+          <form onSubmit={(e) => {
+              e.preventDefault()
+              setSrc(url)
+          }}
+          >
+          <input width={400} height={80} name="url" value={url} type="text" onChange={handleOnChange} />
+          </form>
+        <iframe width={1280} height={480} title="iframe" src={src} />
       </header>
     </div>
   );
