32d387325d5917cc486ad01b19910c41b94a40d5
hung_nguyendang <hung.nguyendang@messagemedia.com>
2022-06-29 12:21:05 +0700

    updated url

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import 'react-tabs/style/react-tabs.css';
 import './App.css';
 
 function App() {
-    const [src, setSrc] = useState('https://iframe-demo-server.herokuapp.com')
-    const [url, setUrl] = useState('https://iframe-demo-server.herokuapp.com')
+    const [src, setSrc] = useState('https://thor-demo-hub.herokuapp.com')
+    const [url, setUrl] = useState('https://thor-demo-hub.herokuapp.com')
     const handleOnChange = (e) => setUrl(e.target.value)
 
     const handleOpenPopup = () => {
-        const newWindow=window.open('https://iframe-demo-server.herokuapp.com', 'Test Popup','height=700,width=1280');
+        const newWindow=window.open('https://thor-demo-hub.herokuapp.com', 'Test Popup','height=700,width=1280');
         if (window.focus) {
             newWindow.focus()
         }
@@ -17,24 +17,24 @@ function App() {
     }
     return (
         <div className="App">
-            <Tabs>
+            <Tabs style={{ margin: "32px" }}>
                 <TabList>
                     <Tab>CSP</Tab>
                     <Tab>Pop-up</Tab>
                 </TabList>
 
-                <TabPanel>
-                    <p>Enter URL</p>
+                <TabPanel style={{ padding: 20 }}>
+                    <p>iFrame URL</p>
                     <form onSubmit={(e) => {
                         e.preventDefault()
                         setSrc(url)
                     }}
                     >
-                        <input style={{ fontSize: "1rem" }} width={400} height={80} name="url" value={url} type="text" onChange={handleOnChange} />
+                        <input style={{ fontSize: "1rem", marginBottom: "16px" }} width={400} height={80} name="url" value={url} type="text" onChange={handleOnChange} />
                     </form>
                     <iframe width={1280} height={480} title="iframe" src={src} />
                 </TabPanel>
-                <TabPanel>
+                <TabPanel style={{ padding: 20 }}>
                     <button onClick={handleOpenPopup} style={{ fontSize: "1rem", padding: "1rem" }}>
                         OPEN
                     </button>
